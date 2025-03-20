07eebb84a99799421c317007202a367f815ea857
TodayCode <aaron_dukes_sw@proton.me>
2025-03-20 22:35:54 +0900

    initial commit

---
+++ renderer.js
@@ -0,0 +1,7 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const btn = document.getElementById('alertBtn');
+    btn.addEventListener('click', () => {
+      alert('Hello! This is an alert.');
+    });
+  });
+  
